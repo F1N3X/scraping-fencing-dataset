3bc393c27bb2505b7e472685aa2540a7ac9a8504
F1N3X <quartzman92@gmail.com>
2026-04-27 15:10:59 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+base_url = "https://www.fencingdatabase.com/?firstname=&lastname=&weapon=&gender=all&tournament=all&year=all&score-fencer=all&opposing-score=all&opposing-lastname=&submit-search=Search+Clips"
+foil_url = base_url.replace("weapon=", "weapon=foil")
+epee_url = base_url.replace("weapon=", "weapon=epee")
+sabre_url = base_url.replace("weapon=", "weapon=sabre")
