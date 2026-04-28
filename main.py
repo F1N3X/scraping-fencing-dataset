6bad597a43ad24d0b9aa5fdf91fc3048a0b5535e
F1N3X <quartzman92@gmail.com>
2026-04-28 10:05:36 +0200

    fix: inverting methods to follow execution order

--- main.py
+++ main.py
@@ -10,31 +10,15 @@ foil_url = base_url + url_params.replace("weapon=", "weapon=foil")
 epee_url = base_url + url_params.replace("weapon=", "weapon=epee")
 sabre_url = base_url + url_params.replace("weapon=", "weapon=sabre")  
 
-
-def normalize_url(url):
-    parsed = urlparse(url)
-    encoded_path = quote(parsed.path)
-    encoded_query = urlencode(parse_qsl(parsed.query, keep_blank_values=True), doseq=True)
-    return urlunparse((parsed.scheme, parsed.netloc, encoded_path, parsed.params, encoded_query, parsed.fragment))
-
-def save_video(video_url, weapon, touch):
-    response = requests.get(video_url, stream=True)
-    if response.status_code == 200:
-        if not os.path.exists(weapon):
-            os.makedirs(weapon)
-        if not os.path.exists(os.path.join(weapon, touch)):
-            os.makedirs(os.path.join(weapon, touch))
-        video_name = video_url.split('/')[-1]
-        video_path = os.path.join(weapon, touch, video_name)
-        with open(video_path, 'wb') as video_file:
-            for chunk in response.iter_content(chunk_size=1024 * 1024):
-                if chunk:
-                    video_file.write(chunk)
-        print(f"Saved video to: {video_path}")
-        return True
-    else:
-        print(f"Failed to download video: {video_url}, status code: {response.status_code}")
-        return False
+def main():
+    foil_datas = page_explorer(base_url, foil_url, "foil")
+    epee_datas = page_explorer(base_url, epee_url, "epee")
+    sabre_datas = page_explorer(base_url, sabre_url, "sabre")
+    print("\n\n")
+    print("--------------------------------------------------\n")
+    print(f"Foil: Downloaded {foil_datas[0]} videos, Failed {foil_datas[1]} downloads, Time taken: {foil_datas[2]:.2f} seconds")
+    print(f"Epee: Downloaded {epee_datas[0]} videos, Failed {epee_datas[1]} downloads, Time taken: {epee_datas[2]:.2f} seconds")
+    print(f"Sabre: Downloaded {sabre_datas[0]} videos, Failed {sabre_datas[1]} downloads, Time taken: {sabre_datas[2]:.2f} seconds")
 
 def page_explorer(base_url, url, weapon):
     call_start_time = time.perf_counter()
@@ -67,12 +51,27 @@ def page_explorer(base_url, url, weapon):
         call_elapsed = time.perf_counter() - call_start_time
     return (downloaded_videos, failed_videos_download, call_elapsed)
 
-def main():
-    foil_datas = page_explorer(base_url, foil_url, "foil")
-    epee_datas = page_explorer(base_url, epee_url, "epee")
-    sabre_datas = page_explorer(base_url, sabre_url, "sabre")
-    print("\n\n")
-    print("--------------------------------------------------\n")
-    print(f"Foil: Downloaded {foil_datas[0]} videos, Failed {foil_datas[1]} downloads, Time taken: {foil_datas[2]:.2f} seconds")
-    print(f"Epee: Downloaded {epee_datas[0]} videos, Failed {epee_datas[1]} downloads, Time taken: {epee_datas[2]:.2f} seconds")
-    print(f"Sabre: Downloaded {sabre_datas[0]} videos, Failed {sabre_datas[1]} downloads, Time taken: {sabre_datas[2]:.2f} seconds")
+def save_video(video_url, weapon, touch):
+    response = requests.get(video_url, stream=True)
+    if response.status_code == 200:
+        if not os.path.exists(weapon):
+            os.makedirs(weapon)
+        if not os.path.exists(os.path.join(weapon, touch)):
+            os.makedirs(os.path.join(weapon, touch))
+        video_name = video_url.split('/')[-1]
+        video_path = os.path.join(weapon, touch, video_name)
+        with open(video_path, 'wb') as video_file:
+            for chunk in response.iter_content(chunk_size=1024 * 1024):
+                if chunk:
+                    video_file.write(chunk)
+        print(f"Saved video to: {video_path}")
+        return True
+    else:
+        print(f"Failed to download video: {video_url}, status code: {response.status_code}")
+        return False
+    
+def normalize_url(url):
+    parsed = urlparse(url)
+    encoded_path = quote(parsed.path)
+    encoded_query = urlencode(parse_qsl(parsed.query, keep_blank_values=True), doseq=True)
+    return urlunparse((parsed.scheme, parsed.netloc, encoded_path, parsed.params, encoded_query, parsed.fragment))
